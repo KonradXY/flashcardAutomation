a28a038da4147a67b2dfb22f237c05d6ef4fe63b
konradXY <corradogiovanniello@hotmail.it>
2019-12-01 16:29:45 +0100

    test per il printer

--- test/main/java/model/TextFileWriterTest.java
+++ test/main/java/model/TextFileWriterTest.java
@@ -1,17 +1,26 @@
 package main.java.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
 import main.java.contracts.IAnkiCard;
 import main.java.contracts.IPrinter;
+import main.java.model.kindle.KindleAnkiCard;
+import main.java.model.languageLearning.LanguageLearningAnkiCard;
 
 @RunWith(JUnitPlatform.class)
 class TextFileWriterTest {
@@ -20,11 +29,47 @@ class TextFileWriterTest {
 	private static final Path filetestpath = Paths.get("./test.txt");
 	private IPrinter printer = new TextFilePrinter();
 	
+	private final static String contentFront = "<div class=\"front\"> this is the front</div>";
+	private final static String contentBack = "<div class=\"back\"> this is the back</div>";
+	private final static String content = contentFront + "\t" + contentBack;
+	
 	@Test
 	void testSimpleCardCorrectlyPrinted() throws IOException {
 		card = new AnkiCard("this is the front", "this is the back");
 		List<IAnkiCard> cardList = Arrays.asList(card);
 		printer.printFile(filetestpath, cardList);
+		assertEquals(content, getContentFromFile(filetestpath));
+	}
+	
+	@Test
+	void testLanguageCardCorrectlyPrinted() throws IOException {
+		card = new LanguageLearningAnkiCard("this is the front", "this is the back");
+		List<IAnkiCard> cardList = Arrays.asList(card);
+		printer.printFile(filetestpath,  cardList);
+		assertEquals(content, getContentFromFile(filetestpath));
+	}
+	
+	@Test
+	void testKindleCardCorrectlyPrinted() throws IOException {
+		card = new KindleAnkiCard("this is the front", "this is the back");
+		List<IAnkiCard> cardList = Arrays.asList(card);
+		printer.printFile(filetestpath,  cardList);
+		assertEquals(content, getContentFromFile(filetestpath));
+	}
+	
+	
+	@AfterAll
+	public static void tearDown() throws IOException {
+		Files.deleteIfExists(filetestpath);
+	}
+	
+	
+	private String getContentFromFile(Path filePath) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath.toString()), "UTF-8"));
+		StringBuilder sb = new StringBuilder();
+		br.lines().forEach(sb::append);
+		br.close();
+		return sb.toString();
 	}
 
 }
