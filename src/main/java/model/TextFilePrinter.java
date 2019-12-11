2cb1df9b0b1c65b26dc5956c53bcb389e0ed227a
Corrado Giovanniello <corrado.giovanniello@edreamsodigeo.com>
2019-12-11 18:16:36 +0100

    refactoring scrittura su file

--- src/main/java/model/TextFilePrinter.java
+++ src/main/java/model/TextFilePrinter.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -19,18 +20,18 @@ public class TextFilePrinter implements IPrinter {
 	}
 
 	@Override  
-	public void printFile(String destPath, List<IAnkiCard> input) {
-		
-		checkOutputFolder(Paths.get(destPath));
-		
-		try (BufferedWriter bos = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(destPath), "UTF-8"))) {
-			for (IAnkiCard ankiCard : input) {
-				bos.write(ankiCard.toString());
-			}
-		} catch (IOException ioex) {
-			System.out.println("Errore nella scrittura del file: " + ioex.getMessage());
-			throw new RuntimeException(ioex);
+	public void printFile(String destFile, List<IAnkiCard> input) {
+
+		Path destPath = Paths.get(destFile);
+		checkOutputFolder(destPath);
+
+		try {
+			Files.write(destPath, (Iterable<String>) input.stream().map(it -> it.toString())::iterator);
+		} catch (IOException ex) {
+			System.out.println("Errore nella scrittura del file: " + ex.getMessage());
+			throw new RuntimeException(ex);
 		}
+
 	}
 	
 }
