23f2e76c6c4b574f6ce50d62a2367273ee142306
Corrado Giovanniello <corrado.giovanniello@edreamsodigeo.com>
2020-01-23 11:58:26 +0100

    commit

--- src/main/java/model/DefaultParser.java
+++ src/main/java/model/DefaultParser.java
@@ -1,8 +1,5 @@
 package main.java.model;
 
-import static main.java.utils.Property.SIMPLE_PARSER_ENG_FIELD;
-import static main.java.utils.Property.SIMPLE_PARSER_ESP_FIELD;
-
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,20 +9,16 @@ import main.java.contracts.IAnkiCard;
 import main.java.contracts.IParser;
 import main.java.strategy.TextParsingStrategy;
 
-/*
- * Classe utilizzata per il parsing di file csv (l'input separator e' il pipe)
- */
 public class DefaultParser implements IParser {
-	
-	@Override
-	public List<IAnkiCard> parse(Map<Path, String> input) {
-		List<IAnkiCard> ankiCards = new ArrayList<>();
-		for (String text : input.values()) {
-			TextParsingStrategy.SIMPLE_PARSER.parse(ankiCards, text);
-		}
-		return ankiCards;
-	}
-	
+
+    @Override
+    public List<IAnkiCard> parse(Map<Path, String> input) {
+        List<IAnkiCard> ankiCards = new ArrayList<>();
+        for (String text : input.values()) {
+            TextParsingStrategy.SIMPLE_PARSER.parseFile(ankiCards, text);
+        }
+        return ankiCards;
+    }
 
 
 }
