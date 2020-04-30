da2de7c7249f727e2b0961820c5da9b386c22cf6
Corrado Giovanniello <corrado.giovanniello@edreamsodigeo.com>
2020-04-30 15:54:14 +0200

    prime classi per il leetcode parser

---
+++ src/main/java/engines/webengines/LeetCodeEngine.java
@@ -0,0 +1,11 @@
+package main.java.engines.webengines;
+
+import main.java.engines.WebCrawlerEngine;
+
+public class LeetCodeEngine extends WebCrawlerEngine {
+
+    @Override
+    public void buildEngine() {
+
+    }
+}
---
+++ src/main/java/model/webcrawler/LeetCodeCrawler.java
@@ -0,0 +1,15 @@
+package main.java.model.webcrawler;
+
+import main.java.contracts.IAnkiCard;
+import main.java.contracts.IWebCrawler;
+
+import java.util.List;
+
+public class LeetCodeCrawler implements IWebCrawler {
+
+    @Override
+    public List<IAnkiCard> createFlashcards(String word) {
+        return null;
+    }
+    
+}
